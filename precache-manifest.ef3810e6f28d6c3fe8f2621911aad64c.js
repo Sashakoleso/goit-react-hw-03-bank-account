57964ff940b188ba6bec6b27c02296881fc4af5d
Alexandr Kolisetsky <skolisetsky@yahoo.com>
2019-12-02 17:05:57 +0200

    Updates

---
+++ precache-manifest.ef3810e6f28d6c3fe8f2621911aad64c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58edec9b8370431b8bc56afe81eee2e4",
+    "url": "/goit-react-hw-03-bank-account/index.html"
+  },
+  {
+    "revision": "333f967c89112cd2a0af",
+    "url": "/goit-react-hw-03-bank-account/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "b41b742f6a7aaa69a510",
+    "url": "/goit-react-hw-03-bank-account/static/css/main.76941797.chunk.css"
+  },
+  {
+    "revision": "333f967c89112cd2a0af",
+    "url": "/goit-react-hw-03-bank-account/static/js/2.aff924e5.chunk.js"
+  },
+  {
+    "revision": "b41b742f6a7aaa69a510",
+    "url": "/goit-react-hw-03-bank-account/static/js/main.709fc57b.chunk.js"
+  },
+  {
+    "revision": "f4863017fedc43fb6d05",
+    "url": "/goit-react-hw-03-bank-account/static/js/runtime-main.f54d16a2.js"
+  }
+]);
